471b249a0d991da80223aa55ae4ccad07fc04a1a
cactoes <maartenhvanrooij@gmail.com>
2024-03-08 15:38:24 +0100

    hashing header

---
+++ src/hash.hpp
@@ -0,0 +1,37 @@
+#ifndef __HASH_HPP__
+#define __HASH_HPP__
+
+#include <stdint.h>
+
+namespace hash {
+    static uint64_t Fnv1a64HashUnsafe(const char* str, uint64_t hash = 14695981039346656037ULL)  {
+        while (*str != '\0') {
+            hash ^= static_cast<uint64_t>(*str);
+            hash *= 1099511628211ULL;
+            ++str;
+        }
+        return hash;
+    }
+
+    uint64_t Fnv1a64Hash(const char* str, uint64_t hash = 14695981039346656037ULL) {
+        int i = 0;
+        while (str[i] != '\0') {
+            hash ^= static_cast<uint64_t>(str[i]);
+            hash *= 1099511628211ULL;
+            i++;
+        }
+        return hash;
+    }
+
+    constexpr uint64_t Fnv1a64HashConst(const char* str, uint64_t hash = 14695981039346656037ULL) {
+        return (*str == '\0') ? hash :
+            Fnv1a64HashConst(str + 1, (hash ^ static_cast<uint64_t>(*str)) * 1099511628211ULL);
+    }
+
+    constexpr uint64_t HashString64Const(const char* str, uint64_t hash = 0ULL) {
+        return (*str == '\0') ? hash :
+            HashString64Const(str + 1, (hash << 1) + static_cast<uint64_t>(*str));
+    }
+}; // namespace hash
+
+#endif // __HASH_HPP__
