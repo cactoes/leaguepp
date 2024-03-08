95c5b689d28314581b6ff9a1b1d68e43063aed7e
cactoes <maartenhvanrooij@gmail.com>
2024-03-08 17:45:57 +0100

    added event manager

---
+++ src/managers/eventmanager.hpp
@@ -0,0 +1,13 @@
+#ifndef __EVENTMANAGER_HPP__
+#define __EVENTMANAGER_HPP__
+
+#include "../browser/browser.hpp"
+
+class event_manager {
+public:
+    void Setup(browser::browser* handle);
+
+    bool HandleWindowEvent(browser::browser* handle, browser::js_args_t args);
+};
+
+#endif // __EVENTMANAGER_HPP__
