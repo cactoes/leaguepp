692f95ddec27cd50e5ac7bdd2fc6bfe2b8be08bd
cactoes <maartenhvanrooij@gmail.com>
2024-03-11 21:46:20 +0100

    added config manager

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,13 @@
 #include "managers/eventmanager.hpp"
 #include "managers/connectormanager.hpp"
 
+#include "managers/configmanager.hpp"
+
 int main() {
     auto browserManager = interface::GetHolder<browser_manager>(from_singleton);
     browserManager->Setup();
 
+    interface::GetHolder<config_manager>(from_singleton)->Setup();
     interface::GetHolder<layout_manager>(from_singleton)->Setup(browserManager->GetHandle());
     interface::GetHolder<event_manager>(from_singleton)->Setup(browserManager->GetHandle());
     interface::GetHolder<connector_manager>(from_singleton)->Setup();
