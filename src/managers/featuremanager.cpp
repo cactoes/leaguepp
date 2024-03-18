7ecd71b32375f99e38ab85002d5106127882fb11
cactoes <maartenhvanrooij@gmail.com>
2024-03-18 17:53:41 +0100

    added more frames

--- src/managers/featuremanager.cpp
+++ src/managers/featuremanager.cpp
@@ -3,10 +3,15 @@
 #include "../interface/holder.hpp"
 #include "layoutmanager.hpp"
 #include "../features/autoAccept.hpp"
+#include "../features/autopicker.hpp"
 
 void feature_manager::Setup() {
     auto frameMain = interface::GetHolder<layout_manager>(from_singleton)->GetFrame();
-    auto lobbyFrame = frameMain->GetComponent<ui::frame>("LobbyFrame");
+    auto targetFrame = frameMain->GetComponent<ui::frame>("HolderFrame");
+
+    auto lobbyFrame = targetFrame->GetComponent<ui::frame>("LobbyFrame");
+    auto autoPickerFrame = targetFrame->GetComponent<ui::frame>("AutoPickerFrame");
 
     CreateFeature<feature::auto_accept>(lobbyFrame);
+    CreateFeature<feature::auto_picker>(autoPickerFrame);
 }
