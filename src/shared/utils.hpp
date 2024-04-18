ea4582c79a94725a65ed02adc097a152050ebfd0
cactoes <maartenhvanrooij@gmail.com>
2024-04-18 14:36:08 +0200

    added ParseNumber

--- src/shared/utils.hpp
+++ src/shared/utils.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <ranges>
 #include <algorithm>
+#include <charconv>
 
 #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 
@@ -20,6 +21,12 @@ static bool StringCompareI(std::string_view a, std::string_view b) {
     return std::ranges::equal(a, b, [](char a, char b) {
         return std::tolower(static_cast<int>(a)) == std::tolower(static_cast<int>(b));
     });
+}
+
+template <typename Ty>
+static bool ParseNumber(std::string_view str, Ty& value) {
+    const auto [ptr, ec] = std::from_chars(str.data(), str.data() + str.size(), value);
+    return ec == std::errc();
 }
 
 #endif // __UTILS_HPP__
