a0d5bc520106cde977db75410e4fcd2e42129ada
cactoes <maartenhvanrooij@gmail.com>
2024-04-18 02:45:40 +0200

    added profile set tier & division

--- src/client/features/profile.cpp
+++ src/client/features/profile.cpp
@@ -1,15 +1,20 @@
 #include "profile.hpp"
 
+#include <iostream>
+#include <utils.hpp>
+
 #include "../interface/holder.hpp"
-
 #include "../managers/connectormanager.hpp"
-
+#include "../managers/configmanager.hpp"
 #include "endpointmappers/summaryplayerdata.hpp"
+#include "endpointmappers/lolchatme.hpp"
 
 #undef interface
 
 void feature::Profile::Setup(std::shared_ptr<ui::Frame> frame, IUiFramework* frameworkApiHandle) {
     auto connectorManager = interface<ConnectorManager>::Get();
+    auto configManager = interface<ConfigManager>::Get();
+    auto cfg = configManager->Get(CONFIG_BASIC);
 
     auto label = frame->AddLabel("disconnected", "left");
 
@@ -57,12 +62,35 @@ void feature::Profile::Setup(std::shared_ptr<ui::Frame> frame, IUiFramework* fra
     //     })
     // );
 
-    // frame->AddComponent<ui::Dropdown>(
-    //     "test single dropdown", false, std::vector<std::string>{ "item1" }, items,
-    //     ui::dropdown_callback([this](std::string, bool, std::vector<std::string> list) {
-    //         return list;
-    //     })
-    // );
+    frame->AddDropdown(
+        "tier", false, std::vector<std::string>{ cfg->GetVar<std::string>("profile::sTier") }, m_tiers,
+        ui::dropdown_callback([this, configManager, cfg](std::string tier, bool, std::vector<std::string> list) {
+            UpdateProfile([tier, configManager, cfg](lolchat::Me& me) {
+                // RANKED_FLEX_SR
+                me.lol->rankedLeagueQueue = "RANKED_SOLO_5x5";
+                me.lol->rankedLeagueTier = tier;
+
+                configManager->TrackedSetVar(cfg, "profile::sTier", tier);
+                return true;
+            });
+
+            return list;
+        })
+    );
+
+    frame->AddDropdown(
+        "division", false, std::vector<std::string>{ cfg->GetVar<std::string>("profile::sDivision") }, m_divisions,
+        ui::dropdown_callback([this, configManager, cfg](std::string division, bool, std::vector<std::string> list) {
+            UpdateProfile([division, configManager, cfg](lolchat::Me& me) {
+                me.lol->rankedLeagueDivision = division;
+
+                configManager->TrackedSetVar(cfg, "profile::sDivision", division);
+                return true;
+            });
+
+            return list;
+        })
+    );
 
     // frame->AddComponent<ui::Selector>(
     //     "test Selector", 1, items,
@@ -77,7 +105,7 @@ void feature::Profile::Setup(std::shared_ptr<ui::Frame> frame, IUiFramework* fra
     // );
 
     connectorManager->AddConnectHandler(
-        client_connect([this, label, frameworkApiHandle]() {
+        client_connect([this, label, frameworkApiHandle, connectorManager]() {
             label->SetText("connected");
             label->Update();
             frameworkApiHandle->CreateNotification("connected", "the client has connected to league");
@@ -95,4 +123,27 @@ void feature::Profile::Setup(std::shared_ptr<ui::Frame> frame, IUiFramework* fra
 
 std::string feature::Profile::GetName() {
     return "profile";
+}
+
+bool feature::Profile::IsValidItem(const std::vector<std::string>& list, const std::string& value) const {
+    return std::ranges::any_of(list, [&](const auto& m_value) {
+        return m_value == value;
+    });
+}
+
+bool feature::Profile::UpdateProfile(std::function<bool(lolchat::Me& me)> setter) {
+    auto connectorManager = interface<ConnectorManager>::Get();
+
+    auto result = connectorManager->MakeRequest(connector::request_type::GET, "/lol-chat/v1/me");
+
+    if (result.status != 200)
+        return false;
+
+    auto data = result.data.get<lolchat::Me>();
+
+    if (!setter(data))
+        return false;
+
+    auto updateResult = connectorManager->MakeRequest(connector::request_type::PUT, "/lol-chat/v1/me", nlohmann::json(data).dump());
+    return updateResult.status == 201;
 }
--- src/client/features/profile.hpp
+++ src/client/features/profile.hpp
@@ -1,13 +1,27 @@
 #ifndef __PROFILE_HPP__
 #define __PROFILE_HPP__
 
+#include <functional>
+
 #include "feature.hpp"
+
+namespace lolchat {
+    struct Me;
+}; // namespace lolchat
 
 namespace feature {
     class Profile : public Feature {
     public:
         void Setup(std::shared_ptr<ui::Frame> frame, IUiFramework* frameworkApiHandle) override;
         std::string GetName() override;
+
+    private:
+        bool IsValidItem(const std::vector<std::string>& list, const std::string& value) const;
+        bool UpdateProfile(std::function<bool(lolchat::Me& me)> setter);
+
+    private:
+        const std::vector<std::string> m_divisions = { "I", "II", "III", "IV" };
+        const std::vector<std::string> m_tiers = { "iron", "bronze", "silver", "gold", "platinum", "emerald", "diamond", "master", "grandmaster", "challenger" };
     };
 }; // namespace feature
 
