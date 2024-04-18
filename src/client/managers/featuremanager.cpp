a9cfbbf01683bd337685d6a3e0cfdf1b571a8b91
cactoes <maartenhvanrooij@gmail.com>
2024-04-19 00:09:58 +0200

    moved frames

--- src/client/managers/featuremanager.cpp
+++ src/client/managers/featuremanager.cpp
@@ -6,11 +6,12 @@
 
 bool FeatureManager::Init(IUiFramework* frameworkApiHandle) {
     auto frameMain = frameworkApiHandle->GetMainFrame();
-    auto targetFrame = frameMain->AddFrame("", ui::FL_VERTICAL);
 
+    auto autoPickerFrame = frameMain->AddFrame("auto picker", ui::FL_VERTICAL_AUTO);
+
+    auto targetFrame = frameMain->AddFrame("", ui::FL_VERTICAL);
+    auto profileFrame = targetFrame->AddFrame("profile", ui::FL_VERTICAL_AUTO);
     auto lobbyFrame = targetFrame->AddFrame("lobby", ui::FL_VERTICAL_AUTO);
-    auto autoPickerFrame = targetFrame->AddFrame("auto picker", ui::FL_VERTICAL_AUTO);
-    auto profileFrame = frameMain->AddFrame("profile", ui::FL_VERTICAL_AUTO);
 
     CreateFeature<feature::LobbyControlls>(lobbyFrame, frameworkApiHandle);
     CreateFeature<feature::AutoPicker>(autoPickerFrame, frameworkApiHandle);
