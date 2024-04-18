f7286641a4f26afee9918036b0fba0a065ce46c4
cactoes <maartenhvanrooij@gmail.com>
2024-04-18 16:46:25 +0200

    cleanup

--- src/uiframework/include/dropdown.hpp
+++ src/uiframework/include/dropdown.hpp
@@ -4,7 +4,7 @@
 #include "component.hpp"
 
 namespace ui {
-    // void DropDownCallback(std::string item, bool newItemState, std::vector<std::string> list)
+    // std::vector<std::string> DropDownCallback(std::string item, bool newItemState, std::vector<std::string> list)
     typedef callback_holder<std::vector<std::string>, std::string, bool, std::vector<std::string>> dropdown_callback;
 
     class Dropdown : public Component {
